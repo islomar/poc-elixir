062b5f8e706d355f3a9732d3510bd6258de7682b
Isidro Lopez <isidro.lopez@helloprima.com>
2026-01-06 17:19:51 +0100

    Remove circular dependency extracting common types

--- elixir-for-programmers-course/hangman/hangman/lib/impl/game.ex
+++ elixir-for-programmers-course/hangman/hangman/lib/impl/game.ex
@@ -2,21 +2,24 @@ defmodule Hangman.Impl.Game do
 
   @type t :: %Hangman.Impl.Game{
           turns_left: integer,
-          game_state: Hangman.state,
-          letters: list(String.t()),
-          used: MapSet.t(String.t())
-        }
+          game_state: Type.state,
+          letters:    list(String.t()),
+          used:       MapSet.t(String.t())
+ }
+
   defstruct(
     turns_left: 7,
     game_state: :initializing,
-    letters: [],
-    used: MapSet.new()
+    letters:    [],
+    used:       MapSet.new()
   )
 
+  @spec new_game() :: t
   def new_game do
     new_game(Dictionary.random_word())
   end
 
+  @spec new_game(String.t) :: t
   def new_game(word) when is_binary(word) do
     %Hangman.Impl.Game{
       letters: word |> String.codepoints
